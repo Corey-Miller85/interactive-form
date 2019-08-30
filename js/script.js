2cd8985dd4479070bbac9e8124fc9f6e5bbdc9c2
Corey Miller <coreymiller353@gmail.com>
2019-08-30 14:59:05 -0400

    Comment out Old

--- js/script.js
+++ js/script.js
@@ -29,29 +29,29 @@ $(document).ready( () => {
     $('#color').prepend(noSelection);
 });
 
-//checks to see if there is a selection, and chooses correct color
-$('#design').on('change', () => {
-    //creates "please select a design" in color box element
-    const noSelection = $('<option selected value="noSelect">Please Make a Selection</option>');
-        //if design shows "select theme" loop through color options and hide them then append the option Please choose, and select it.
-    if ($('#design').val() === "Select Theme") {
-        $('#color option').each(function(index, element){
-            $(element).hide();
-        });
-        $('#color').prepend(noSelection);
-    } else if ($('#design').val() === "js puns") {
-        //if a design is selected, show the color options, hide the choose design option, and then select a valid color.
-        $('#color option').each(function (index, element) {
-            if ($(element).val() === "noSelect") {
-                $(element).hide();
-            } else if ($(element).val() === 'cornflowerblue') { // jumps noSelect and marks the next as selected by default.
-                $(element).show();
-                $(element).attr('selected', true);
-            } else if ($(element).val() === 'darkslategrey' || 'gold') { //shows the rest of the colors.
-                $(element).show();
-            } else {
-                $(element).hide();
-            }
-        });
-    }
-});
+// //checks to see if there is a selection, and chooses correct color
+// $('#design').on('change', () => {
+//     //creates "please select a design" in color box element
+//     const noSelection = $('<option selected value="noSelect">Please Make a Selection</option>');
+//         //if design shows "select theme" loop through color options and hide them then append the option Please choose, and select it.
+//     if ($('#design').val() === "Select Theme") {
+//         $('#color option').each(function(index, element){
+//             $(element).hide();
+//         });
+//         $('#color').prepend(noSelection);
+//     } else if ($('#design').val() === "js puns") {
+//         //if a design is selected, show the color options, hide the choose design option, and then select a valid color.
+//         $('#color option').each(function (index, element) {
+//             if ($(element).val() === "noSelect") {
+//                 $(element).hide();
+//             } else if ($(element).val() === 'cornflowerblue') { // jumps noSelect and marks the next as selected by default.
+//                 $(element).show();
+//                 $(element).attr('selected', true);
+//             } else if ($(element).val() === 'darkslategrey' || 'gold') { //shows the rest of the colors.
+//                 $(element).show();
+//             } else {
+//                 $(element).hide();
+//             }
+//         });
+//     }
+// });
